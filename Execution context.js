1574226a26a008be1d3fa424fdc6b730b30fa116
OmrajeAmbure <omrajeambure27@gmail.com>
2024-08-31 22:32:54 +0530

    it will store the all code of javascript

---
+++ Execution context.js
@@ -0,0 +1,10 @@
+debugger
+
+add(); // it wll when memory execution context phase . it will store all the code of this function inside the memory execution phase.
+console.log(n)
+// it will be executed in the memory execution phase . it will store all the code of this function
+// inside the memory execution phase . 
+var n = 21;
+function add(){
+    sum = n + n;
+}
